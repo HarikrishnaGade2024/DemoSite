da2f20152a5a7677a6ace78abc53cbdf5037af77
harigade <hari@haris-MBP.attlocal.net>
2024-06-08 21:25:00 -0700

    Init

---
+++ src/main/java/com/example/demo/ApiPracticeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPracticeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPracticeDemoApplication.class, args);
+	}
+
+}
